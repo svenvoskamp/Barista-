11ebbbdafa9382441623d9f8532de3fd0ff367ca
svenvoskamp <sven.voskamp@student.howest.be>
2019-10-22 14:06:09 +0200

    Barista Hardcoded

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './style.css';
+
+{
+  const showCoffees = async () => {
+
+    const response = await fetch(`./assets/data/coffees.json`);
+    const coffees = await response.json();
+    console.log(coffees);
+  };
+
+
+  const init = () => {
+    showCoffees();
+  };
+
+  init();
+
+}
