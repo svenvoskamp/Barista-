9d4b52d2206aa71cd9d2d47a27517fb4ae7eb208
svenvoskamp <sven.voskamp@student.howest.be>
2019-10-23 22:06:31 +0200

    bug fixes

--- src/js/script.js
+++ src/js/script.js
@@ -7,8 +7,8 @@
       const $div = document.createElement(`div`);
       $div.classList.add(`emptystate`);
       $div.innerHTML =`
-      <img srcset="./assets/img/coffee-maker.jpg 67w,
-        ./assets/img/coffee-maker@2x.jpg 134w" sizes="67px" src="./assets/img/coffee-maker.jpg" alt="A coffee maker">
+      <img srcset="./src/assets/img/coffee-maker.jpg 67w,
+        ./src/assets/img/coffee-maker@2x.jpg 134w" sizes="67px" src="./src/assets/img/coffee-maker.jpg" alt="A coffee maker">
 
       <span class="emptystate__content">
         Please order something
@@ -34,7 +34,7 @@
   }
 
   const fetchJSON = () => {
-    fetch('../src/assets/data/coffees.json')
+    fetch('./src/assets/data/coffees.json')
       .then(response => {
         return response.json();
       })
