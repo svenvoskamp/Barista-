2ca098b64926ccbb93d42d79ce663e7aceb00714
svenvoskamp <sven.voskamp@student.howest.be>
2019-10-24 20:46:07 +0200

    coffeemaker fix

--- src/js/script.js
+++ src/js/script.js
@@ -8,9 +8,8 @@
       const $div = document.createElement(`div`);
       $div.classList.add(`emptystate`);
       $div.innerHTML =`
-      <img srcset="../assets/img/coffee-maker.jpg 67w,
-        ../assets/img/coffee-maker@2x.jpg 134w" sizes="67px" src="../assets/img/coffee-maker.jpg" alt="A coffee maker">
-
+      <img srcset="./src/assets/img/coffee-maker.jpg 67w,
+       ./src/assets/img/coffee-maker@2x.jpg" 134w" sizes="67px, 134px" src="./src/assets/img/coffee-maker.jpg" alt="A coffee maker">
       <span class="emptystate__content">
         Please order something
         <span role="img" aria-label="Drunk emoji">
